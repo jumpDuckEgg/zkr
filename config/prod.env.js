1b5585744d50c443bac2723d407434eb69d1c23f
jumpDuck <1197369402@qq.com>
2018-04-23 11:33:58 +0800

    #28 添加线上配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://localhost:9998/zkr/page"',
+  BASE_API: '"http://192.168.0.28:9998/zkr/page"',
 }
--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    BASE_URL: 'http://182.61.47.252:9998/zkr/page',
+    // BASE_URL: 'http://182.61.47.252:9998/zkr/page',
+    BASE_URL: 'http://192.168.0.28:9998/zkr/page',
 }
